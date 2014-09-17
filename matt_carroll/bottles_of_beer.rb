f9c602294eac1f68aef26cdc91ac5856a9d753ce
Matt Carroll <matt@fail-harder.com>
2014-09-17 19:13:12 -0400

    Adding bottles_of_beer.rb git add bottles_of_beer.rb

---
+++ matt_carroll/bottles_of_beer.rb
@@ -0,0 +1,16 @@
+# 99 Bottles of Beer on the Wall Exercise
+#
+# Write a program that prints 99 bottles of beer on the wall. The song goes as
+# so:
+#
+# 99 bottles of beer on the wall
+# 99 bottles of beer!
+# You take one down and pass it around,
+# 98 bottles of beer on the wall!
+# ...
+#
+# And ends with 
+# 1 bottle of beer on the wall
+# 1 bottle of beer!
+# You take one down and pass it around,
+# No more bottles of beer on the wall
