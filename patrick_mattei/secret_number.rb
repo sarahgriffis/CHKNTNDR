b4f0ac7d379c9c991db9bc719124522b011cfb7e
Patrick Mattei <patrick.mattei@gmail.com>
2014-09-18 16:32:37 -0400

    Finished secret nubmer homework

--- patrick_mattei/secret_number.rb
+++ patrick_mattei/secret_number.rb
@@ -3,29 +3,49 @@
 # We're ready to program! To practice our Ruby skills lets create a secret
 # number game. In this game players have three tries to guess a secret number
 # between 1 and 10.
-#
-# Here are some guidelines to building your game:
-#
-#   Intros
-#   - Welcome the player to your game. Let them know who created the game.
+
+puts "Hi!  My name is Patrick.  Welcome to the number game."
 #   - Ask for the player's name then personally greet them by printing to the
 #     screen, "Hi player_name!"
-#   - Any good game will communicate the rules. Let the player know they must
-#     guess a number between 1 and 10 and that they only have 3 tries to do so.
-#
-#   Functionality:
-#     - Manually specify the secret number in a variable. Choose a random 
-#       number between 1 and 10.
-#     - Ask the user for their guess.
-#     - Verify if they were correct. If the player guesses correctly they win
-#       the game, they should be congratulated and the game should end.
-#     - If they guess incorrectly, give the player some direction. If they
-#       guess too high let them know, if they guess too low, let them know.
-#     - Don't forget to let your players know how many guesses they have left.
-#       Your game should say something like:
-#         "You have X guesses before the game is over enter a another number"
-#     - If they don't guess correctly after 3 tries, display that the player
-#       lost and that the game is over. Be nice and let them know what the
-#       `secret_number` was.
-#
-###############################################################################
+puts "What's your name?"
+player_name =  gets.strip
+puts "Hi #{player_name}!"
+puts
+
+puts "Rules: Guess a number between 1 and 10.  You only have 3 tries to do so."
+tries_left = 3
+
+secret_number = 7
+
+winner = false
+
+puts "Type a guess:"
+
+until tries_left == 0 or winner == true 
+  guess = gets.strip.to_i
+
+  if guess == secret_number
+    winner = true
+    puts "Congratulations!!  You win!"
+  
+
+  elsif guess > secret_number
+    tries_left -= 1
+      puts "Your guess was too high."
+
+  elsif guess < secret_number
+    tries_left -= 1
+    puts "Your guess was too low."
+  end
+
+  if tries_left > 1 and winner == false 
+    puts "You have #{tries_left} guesses before the game is over.  Enter  another number."
+  elsif tries_left == 1 and winner == false 
+    puts "You have #{tries_left} guess before the game is over.  Enter  another number."
+  end
+end
+
+
+if tries_left == 0 and winner == false 
+  puts "Sorry, #{player_name}, you lost.  The secret number was #{secret_number}"
+end
