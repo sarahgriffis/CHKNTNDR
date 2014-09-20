f05b058e6db00c55fd8d6047762998c4018dc1f8
Brandon Marshall Ngai <brandonmarshallngai@gmail.com>
2014-09-20 12:15:36 -0400

    Secret_Number FINAL

--- brandon_ngai/secret_number.rb
+++ brandon_ngai/secret_number.rb
@@ -35,44 +35,47 @@ def nameplayer
   gets.strip
 end
 
-def game
-
-  secret_number = 4
-
-  user_number = gets.strip.to_i
-
-  if user_number == secret_number
-    puts 'How about that? You got it right!!!'
-  end
-
-  if user_number > secret_number
-    puts 'Whoa, slow down there, cowboy. Think a little smaller.'
-    game
-  end
-
-  if user_number < secret_number
-    puts 'Don\'t be afraid to dream a little bigger, darling.'
-    game
-  end
-
-  if user_number != Integer
-    puts 'Did I say this was the secret \"LETTER\" game?? NO!'
-    game
-  end
-end
-
-
 puts 'Hello there! Welcome to the Secret Number game, created by Brandon.'
 puts 'What\'s your name?'
 name = nameplayer
 puts "Great to meet you, #{name}!"
+sleep 1
 puts 'Let\'s play the Secret Number game! The rules are pretty simple.'
+sleep 1
 puts 'You\'ll have THREE chances to guess the number that I\'m thinking of,'
 puts 'which is a number between 1 and 10.'
+sleep 1
 puts 'Are you ready to begin? Type in a number!'
 
-game
 
-puts 'THE END.'
+secret_number = 4
+
+
+3.downto(1) do |n|
+
+  user_number = gets.strip.to_i
+
+    if user_number > secret_number && user_number <= 10
+      puts "Whoa, slow down there, cowboy. Think a little smaller. #{n-1} more tries."
+    end
+
+    if user_number < secret_number && user_number >= 1
+      puts "Don\'t be afraid to dream a little bigger, darling. #{n-1} more tries."
+    end
+
+    if user_number <= 0 || user_number >= 11
+      puts "IT\'S GOTTA BE BETWEEN 1 AND 10!!! #{n-1} more tries."
+    end
+
+    if user_number == secret_number
+      puts "YES! I was thinking of 4!"
+      puts "Congratulations, #{name}! You\'ve won the game."
+      exit
+    end
+end
+
+puts "Tough shit, loser. The number was #{secret_number}!"
+
+
 
   
