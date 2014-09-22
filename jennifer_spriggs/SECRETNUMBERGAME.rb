9d487c69cf2e1553e94d91adee15b69708dccb71
Jennifer Spriggs <jenniferlspriggs@gmail.com>
2014-09-22 18:35:26 -0400

    IGNORE: Still has an issue

---
+++ jennifer_spriggs/SECRETNUMBERGAME.rb
@@ -0,0 +1,83 @@
+###############################################################################
+#
+# We're ready to program! To practice our Ruby skills lets create a secret
+# number game. In this game players have three tries to guess a secret number
+# between 1 and 10.
+#
+# Here are some guidelines to building your game:
+#
+#   Intros
+#   - Welcome the player to your game. Let them know who created the game.
+#   - Ask for the player's name then personally greet them by printing to the
+#     screen, "Hi player_name!"
+#   - Any good game will communicate the rules. Let the player know they must
+#     guess a number between 1 and 10 and that they only have 3 tries to do so.
+#
+#   Functionality:
+#     - Manually specify the secret number in a variable. Choose a random 
+#       number between 1 and 10.
+#     - Ask the user for their guess.
+#     - Verify if they were correct. If the player guesses correctly they win
+#       the game, they should be congratulated and the game should end.
+#     - If they guess incorrectly, give the player some direction. If they
+#       guess too high let them know, if they guess too low, let them know.
+#     - Don't forget to let your players know how many guesses they have left.
+#       Your game should say something like:
+#         "You have X guesses before the game is over enter a another number"
+#     - If they don't guess correctly after 3 tries, display that the player
+#       lost and that the game is over. Be nice and let them know what the
+#       `secret_number` was.
+#
+###############################################################################
+
+class SecretNumberGame
+  
+  def initialize
+    @secret_number = 22
+    @guesses = 7
+  end
+
+  def play
+    greeting
+    @guesses.times do |n|
+      guesses_left = n-1 
+      puts "\nYou have #{guesses_left} guesses left!"
+      puts "Please make your first guess:"
+      players_guess = $stdin.gets.chomp.to_i
+      return if correct?(players_guess)
+    end
+    puts "You lost! :-( The number to guess was #{@secret_number}."
+  end
+    
+  def greet(player_name)
+    puts "Welcome to the Secret Number Game, #{first_name}!" 
+  end
+
+  def created_by
+    puts "Created by Jennifer Spriggs."  
+  end
+
+
+  def correct?(players_guess)
+    if ___
+      puts "You win! The number was #{@secret_number}."
+      return true
+    elsif ___
+      puts "Make your next guess higher than #{players_guess}"
+    elsif ___
+      puts "Make your next guess lower than #{players_guess}"
+    end
+    false
+  end
+
+  #Do not change anything past this line
+  private 
+
+  def greeting
+    puts created_by
+    puts "\nWhat is your name?"
+    player_name = gets.strip
+    puts greet(player_name)
+  end
+
+end
