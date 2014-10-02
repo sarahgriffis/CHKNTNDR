1f6b84793f6e2554204bb62aa4dae0e43b714e0c
Brooks Swinnerton <bswinnerton@gmail.com>
2014-10-01 20:00:46 -0400

    Removing answers to Lesson 7

--- chenoe_hart/apartment/lib/apartment.rb
+++ chenoe_hart/apartment/lib/apartment.rb
@@ -1,10 +1,2 @@
 class Apartment
-  attr_accessor :number, :renter, :rent
-
-  def initialize(number, square_feet, bedrooms, bathrooms)
-    @number = number
-    @square_feet = square_feet
-    @bedrooms = bedrooms
-    @bathrooms = bathrooms
-  end
 end
