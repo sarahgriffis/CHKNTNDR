4b21b8712b9660a446a6b1c6059dda506f8ed81b
Sarah Griffis <sgriffis@paperlesspost.com>
2014-11-13 17:27:17 -0500

    Fixing sign up email url.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    @url  = 'http://localhost:3000/users/sign_in'
+    @url  = 'https://aqueous-anchorage-2260.herokuapp.com/users/sign_in'
     mail(to: @user.email, subject: 'Welcome to CHKTNDR')
   end
 end
