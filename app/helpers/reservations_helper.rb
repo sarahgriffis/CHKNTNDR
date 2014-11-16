d7a192d6f7471b3d433a529f1abc674dac283fca
Sarah Griffis <sgriffis@paperlesspost.com>
2014-11-16 16:19:07 -0500

    Adding default restaurant picture

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -6,11 +6,19 @@ module ReservationsHelper
      <br>
      Rating:  #{r.rating} from #{r.number_of_ratings} #{'rating'.pluralize}.
      <br>
-     <img src=#{r.photo_url}>
+     <img src=#{photo_url(r)}>
      <br>
      <p class='justify restauranttext'>#{r.formatted_address}</p>
      #{r.formatted_phone}
      <hr>
      </div>"
+  end
+
+  def photo_url(reservation)
+    if reservation.photo_url
+      reservation.photo_url
+    else
+      asset_path('restaurant-clip-art.jpg')
+    end
   end
 end
