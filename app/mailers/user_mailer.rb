5d49d51b58762014caae780e98ee3fb072538984
Sarah Griffis <sgriffis@paperlesspost.com>
2014-12-15 20:23:24 -0500

    Fix typo in mailer.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-    default from: 'CHKTNDR@CHKNTNDR.com'
+    default from: 'CHKNTNDR@CHKNTNDR.com'
 
   def welcome_email(user)
     @user = user
     @url  = 'https://aqueous-anchorage-2260.herokuapp.com/users/sign_in'
-    mail(to: @user.email, subject: 'Welcome to CHKTNDR')
+    mail(to: @user.email, subject: 'Welcome to CHKNTNDR')
   end
 end
